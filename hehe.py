1f709077cf4e3b2a955c73dc1d86184493764ce0
trung_pc_l <trungnd2767@gmail.com>
2024-07-27 00:50:49 +0700

    oke2

--- hehe.py
+++ hehe.py
@@ -1,4 +1,4 @@
 print("Xin chao cac ban")
 print("Dev01 them mot dong")
 print("kysutrung them mot dong hehehe")
-print("kysutrung them mot dong 2 hehehe")
+print("kysutrung them mot dong 3 hehehe")
